2f95d6ee00fdbaa609105f0565adc84af09fc6c9
xbrooke <554719247@qq.com>
2023-03-04 14:23:38 +0800

    vite

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,11 +1,15 @@
-import DefaultTheme from 'vitepress/theme'
+import { h } from 'vue'
+import Theme from 'vitepress/theme'
 import './style/overwrite.css'
 import './style/style.css'
-const theme = {
-  ...DefaultTheme,
-  enhanceApp({ app }) {
-
+import './style/vars.css'
+import './custom.css'
+export default {
+  ...Theme,
+  Layout() {
+    return h(Theme.Layout, null, {
+      'home-features-after': () => h(HomeSponsors),
+      'aside-ads-before': () => h(AsideSponsors),
+    })
   }
 }
-
-export default theme
