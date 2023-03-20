17bb6e6b9c99bd81ce426c78ee91181d814d4dd5
xbrooke <107459849+xbrooke@users.noreply.github.com>
2023-03-20 05:33:25 +0000

    Deploying to gh-pages from  @ d0c41606cb1dcc5d5b83d903f8b0c6a73567274c 🚀

--- assets/brooke2_02.md.69ad7e77.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as t,c as o}from"./app.7b800a82.js";const m=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"brooke2/02.md"}'),r={name:"brooke2/02.md"};function a(s,c,n,_,p,d){return t(),o("div")}const f=e(r,[["render",a]]);export{m as __pageData,f as default};
